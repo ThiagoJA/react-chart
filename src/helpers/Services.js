f0932b9df8f05ffd65b5d4058f24e5cc74d79d31
Thiago Jesuino <thiago@enesolucoes.com.br>
2020-02-01 23:25:39 -0300

    Up Heroku

--- src/helpers/Services.js
+++ src/helpers/Services.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 export const GetEmployees = (setFunc) => {
-  axios.get('http://localhost:3000/employees').then(res => setFunc(res))
+  axios.get('https://node-employees.herokuapp.com/employees').then(res => setFunc(res))
   return void 0;
 }
 
 export const PostEmployees = (params) => {
-  axios.post('http://localhost:3000/employees', params)
-  window.location.reload();
+  axios.post('https://node-employees.herokuapp.com/employees', params)
+  setTimeout(()=>{
+    window.location.reload();
+
+  },1000)
   return void 0;
 }
